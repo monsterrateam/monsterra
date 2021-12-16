872dd1a49722f1f0ba8a96ccbc7ad226e9013c3f
Nguyen Dai Duong <duongnd@Nguyens-MacBook-Pro-2.local>
2021-12-16 16:27:27 +0700

    add,remove, get minter list

--- contracts/MAGToken.sol
+++ contracts/MAGToken.sol
@@ -6,28 +6,54 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract MAGToken is ERC20, Ownable {
 
-    mapping (address => bool ) public whiteList;
+    mapping (address => bool ) public minter;
+
+    address[] private minterList;
     
     constructor() ERC20("MAG Token", "MAG") {
-        //Whitelist owner
-        whiteList[msg.sender] = true;
+        //add owner to minterList
+        addMinter(msg.sender);
     }
 
-    modifier onlyWhiteList() {
-        require(whiteList[msg.sender], "Only-whitelist-minter");
+    modifier onlyMinter() {
+        require(minter[msg.sender], "Only-minter");
         _;
     }
 
-    function adminWhiteList(address _whitelistAddr, bool _whiteList) onlyOwner public {
-        whiteList[_whitelistAddr] = _whiteList;
-    }
-  
-    function mint(address to, uint256 amount) public onlyWhiteList{
+    function mint(address to, uint256 amount) public onlyMinter{
         _mint(to, amount);
     }
 
     function burn(uint256 amount) public virtual {
         _burn(msg.sender, amount);
+    }
+
+    function addMinter(address _minterAddr) public onlyOwner{
+        require(!minter[_minterAddr], "Is minter");
+        minterList.push(_minterAddr);
+        minter[_minterAddr] = true;
+    }
+
+    function removeMinter(address _minterAddr) public onlyOwner{
+        require(minter[_minterAddr], "Not minter");
+        minter[_minterAddr] = false;
+        
+        uint256 i = 0;
+        address _minter;
+        while (i < minterList.length) {
+            _minter = minterList[i];
+            if (!minter[_minter]) {
+                minterList[i] = minterList[minterList.length - 1];
+                delete minterList[minterList.length - 1];
+                minterList.pop();
+            } else {
+                i++;
+            }
+        }
+    }
+
+    function getMinters() public view returns (address[] memory){
+        return minterList;
     }
     
 }
